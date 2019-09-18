40c893c84cd6ab1208557891497184a6a36c8b77
vasishtaballa <vasishta.balla@gmail.com>
2019-09-19 00:23:04 +0530

    Solved SpiralOrderMatrixII

--- src/com/academy/homework/sep17/SpiralOrderMatrixII.java
+++ src/com/academy/homework/sep17/SpiralOrderMatrixII.java
@@ -1,5 +1,7 @@
 package com.academy.homework.sep17;
 
+// Question: https://www.interviewbit.com/problems/spiral-order-matrix-ii/
+
 public class SpiralOrderMatrixII {
 	public static void main(String[] args) {
 		SpiralOrderMatrixII obj = new SpiralOrderMatrixII();
@@ -8,27 +10,29 @@ public class SpiralOrderMatrixII {
 
 	public int[][] generateMatrix(int A) {
 		int[][] matrix = new int[A][A];
-		int i = 0, j = A - 1, k = A - 1, l = A - 1, number = 1;
-		// Traverse left to right
-		for (int m = i; m <= j; m++) {
-			matrix[i][m] = number++;
+		int i = 0, j = A - 1, k = A - 1, l = 0, number = 1;
+		while (l <= j && i <= k) {
+			// Traverse left to right
+			for (int m = i; m <= j; m++) {
+				matrix[i][m] = number++;
+			}
+			i += 1;
+			// Traverse from top to bottom
+			for (int m = i; m <= k; m++) {
+				matrix[m][j] = number++;
+			}
+			j -= 1;
+			// Traverse from right to left
+			for (int m = j; m >= l; m--) {
+				matrix[k][m] = number++;
+			}
+			k -= 1;
+			// Traverse from bottom to top
+			for (int m = k; m >= i; m--) {
+				matrix[m][l] = number++;
+			}
+			l += 1;
 		}
-		i += 1;
-		// Traverse from top to bottom
-		for (int m = j; m <= k; m++) {
-			matrix[m][j] = number++;
-		}
-		j -= 1;
-		// Traverse from right to left
-		for (int m = k; m <= l; m--) {
-			matrix[k][m] = number++;
-		}
-		k -= 1;
-		// Traverse from bottom to top
-		for (int m = l; m <= i; m--) {
-			matrix[m][l] = number++;
-		}
-		l += 1;
 		return matrix;
 	}
 }
