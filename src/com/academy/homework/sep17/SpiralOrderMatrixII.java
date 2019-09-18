6ca20327e94b2fd934c21d48bf9e9093e0d171e4
vasishtaballa <vasishta.balla@gmail.com>
2019-09-19 00:24:46 +0530

    Removed unwanted braces

--- src/com/academy/homework/sep17/SpiralOrderMatrixII.java
+++ src/com/academy/homework/sep17/SpiralOrderMatrixII.java
@@ -13,24 +13,20 @@ public class SpiralOrderMatrixII {
 		int i = 0, j = A - 1, k = A - 1, l = 0, number = 1;
 		while (l <= j && i <= k) {
 			// Traverse left to right
-			for (int m = i; m <= j; m++) {
+			for (int m = i; m <= j; m++)
 				matrix[i][m] = number++;
-			}
 			i += 1;
 			// Traverse from top to bottom
-			for (int m = i; m <= k; m++) {
+			for (int m = i; m <= k; m++)
 				matrix[m][j] = number++;
-			}
 			j -= 1;
 			// Traverse from right to left
-			for (int m = j; m >= l; m--) {
+			for (int m = j; m >= l; m--)
 				matrix[k][m] = number++;
-			}
 			k -= 1;
 			// Traverse from bottom to top
-			for (int m = k; m >= i; m--) {
+			for (int m = k; m >= i; m--)
 				matrix[m][l] = number++;
-			}
 			l += 1;
 		}
 		return matrix;
