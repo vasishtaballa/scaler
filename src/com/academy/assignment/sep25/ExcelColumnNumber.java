c5771c7e336be1d10fe34f928416121104578d5a
vasishtaballa <vasishta.balla@gmail.com>
2019-09-26 01:15:25 +0530

    Solved ExcelColumnNumber

---
+++ src/com/academy/assignment/sep25/ExcelColumnNumber.java
@@ -0,0 +1,21 @@
+package com.academy.assignment.sep25;
+
+// Question: https://www.interviewbit.com/problems/excel-column-number/
+
+public class ExcelColumnNumber {
+
+	public static void main(String[] args) {
+		ExcelColumnNumber obj = new ExcelColumnNumber();
+		System.out.println(obj.titleToNumber("AA"));
+	}
+
+	public int titleToNumber(String A) {
+		int result = 0;
+		for (int i = A.length() - 1, index = 0; i > -1; i--, index++) {
+			char ch = A.charAt(i);
+			result += Math.pow(26, index) * (ch - 64);
+		}
+		return result;
+	}
+
+}
