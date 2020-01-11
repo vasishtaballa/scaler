f604a28281a3f1fdaadaf9d197ffeb2652af0c47
vasishtaballa <vasishta.balla@gmail.com>
2020-01-11 14:15:15 +0530

    Solved Inversions

--- src/com/academy/assignment/nov15/Inversions.java
+++ src/com/academy/assignment/nov15/Inversions.java
@@ -4,6 +4,8 @@ package com.academy.assignment.nov15;
 
 public class Inversions {
 
+	int count = 0;
+
 	public static void main(String[] args) {
 		Inversions obj = new Inversions();
 		int[] array = { 2, 4, 1, 3, 5 };
@@ -11,21 +13,42 @@ public class Inversions {
 	}
 
 	public int countInversions(int[] A) {
-		// mergeSortUtil(A, 0, A.length - 1, 0);
-		return 1;
+		divideArray(A, 0, A.length - 1);
+		return count;
 	}
 
-	private void mergeSortUtil(int[] array, int low, int high) {
-		if (high < low)
-			return;
-		int mid = low + (high - low) / 2;
-		mergeSortUtil(array, low, mid - 1);
-		mergeUtil(array, low, high);
-		mergeSortUtil(array, mid + 1, high);
+	private void divideArray(int[] array, int low, int high) {
+		if (low < high) {
+			int mid = low + (high - low) / 2;
+			divideArray(array, low, mid);
+			divideArray(array, mid + 1, high);
+			combineArray(array, low, high, mid);
+		}
 	}
 
-	private void mergeUtil(int[] array, int low, int high) {
+	private void combineArray(int[] array, int low, int high, int mid) {
+		int[] left = new int[mid - low + 1];
+		int[] right = new int[high - mid];
+		for (int i = 0; i < left.length; i++)
+			left[i] = array[i + low];
+		for (int j = 0; j < right.length; j++)
+			right[j] = array[mid + j + 1];
+		int i = 0, j = 0, k = low;
+		while (i < left.length && j < right.length) {
+			if (left[i] <= right[j]) {
+				array[k++] = left[i];
+				i++;
+			} else {
+				array[k++] = right[j];
+				j++;
+				count += left.length - i;
+			}
+		}
 
+		while (i < left.length)
+			array[k++] = left[i++];
+		while (j < right.length)
+			array[k++] = right[j++];
 	}
 
 }
