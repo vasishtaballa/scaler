ad0cbbf353e2cb29b2073541a406d7553b40124a
vasishtaballa <vasishta.balla@gmail.com>
2020-02-23 23:16:26 +0530

    ZigZagLOT and Print Matrix utility

--- src/com/academy/assignment/util/PrintUtility.java
+++ src/com/academy/assignment/util/PrintUtility.java
@@ -12,4 +12,12 @@ public class PrintUtility {
 		for (Object item : list)
 			System.out.print(item + " ");
 	}
+
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++)
+				System.out.print(matrix[i][j] + " ");
+			System.out.println();
+		}
+	}
 }
