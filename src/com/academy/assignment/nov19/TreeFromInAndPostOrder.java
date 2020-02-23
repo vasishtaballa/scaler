fab9576d0fa618e7b145abb71f67d030ff782746
vasishtaballa <vasishta.balla@gmail.com>
2020-02-23 19:25:50 +0530

    HBT and Tree Identical Problem

--- src/com/academy/assignment/nov19/TreeFromInAndPostOrder.java
+++ src/com/academy/assignment/nov19/TreeFromInAndPostOrder.java
@@ -9,29 +9,29 @@ public class TreeFromInAndPostOrder {
 
 	public static void main(String[] args) {
 		TreeFromInAndPostOrder obj = new TreeFromInAndPostOrder();
-		int[] inOrder = { 4, 2, 5, 1, 6, 3, 7 };
-		int[] postOrder = { 4, 5, 2, 6, 7, 3, 1 };
+		int[] inOrder = { 4, 2, 5, 1, 3 };
+		int[] postOrder = { 4, 5, 2, 3, 1 };
 		TreeNode root = obj.buildTree(inOrder, postOrder);
 		TreeUtility.printInorder(root);
 	}
 
 	public TreeNode buildTree(int[] A, int[] B) {
-		Index preIndx = new Index();
-		preIndx.index = A.length - 1;
-		return buildTreeUtil(A, B, 0, A.length - 1, preIndx);
+		Index postIndx = new Index();
+		postIndx.index = A.length - 1;
+		return buildTreeUtil(A, B, 0, A.length - 1, postIndx);
 	}
 
-	private TreeNode buildTreeUtil(int[] inOrder, int[] postOrder, int inStart, int inEnd, Index preIndex) {
+	private TreeNode buildTreeUtil(int[] inOrder, int[] postOrder, int inStart, int inEnd, Index postIndex) {
 		if (inStart > inEnd)
 			return null;
-		TreeNode node = new TreeNode(postOrder[preIndex.index]);
-		preIndex.index--;
+		TreeNode node = new TreeNode(postOrder[postIndex.index]);
+		postIndex.index--;
 		int inIndex = getInIndex(inOrder, node.val);
 		if (inIndex == -1)
 			return null;
 		else {
-			node.right = buildTreeUtil(inOrder, postOrder, inIndex + 1, inEnd, preIndex);
-			node.left = buildTreeUtil(inOrder, postOrder, inStart, inIndex - 1, preIndex);
+			node.right = buildTreeUtil(inOrder, postOrder, inIndex + 1, inEnd, postIndex);
+			node.left = buildTreeUtil(inOrder, postOrder, inStart, inIndex - 1, postIndex);
 		}
 		return node;
 	}
