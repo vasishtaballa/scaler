78fe2dd134ae4cd6b25e7ff097aa02944e649ca8
vasishtaballa <vasishta.balla@gmail.com>
2020-03-28 10:28:10 +0530

    CommonNodesInBST v2

--- src/com/academy/homework/nov21/CommonNodesInBST.java
+++ src/com/academy/homework/nov21/CommonNodesInBST.java
@@ -1,6 +1,6 @@
 package com.academy.homework.nov21;
 
-import java.util.ArrayList;
+import java.util.Stack;
 
 import com.academy.assignment.util.GeneralUtility;
 import com.academy.assignment.util.TreeNode;
@@ -12,8 +12,8 @@ public class CommonNodesInBST {
 
 	public static void main(String[] args) {
 		CommonNodesInBST obj = new CommonNodesInBST();
-		int[] arr1 = { 5, 2, 8, -1, 3, -1, 15, 7, -1 };
-		int[] arr2 = { 7, 1, 10, -1, 2, -1, 15, 8, -1 };
+		int[] arr1 = { 5, 2, 8, -1, 3, -1, 15, -1, -1, 7, -1 };
+		int[] arr2 = { 7, 1, 10, -1, 2, -1, 15, -1, -1, 8, -1 };
 		String[] array1 = GeneralUtility.getStrFrmInt(arr1);
 		String[] array2 = GeneralUtility.getStrFrmInt(arr2);
 		TreeNode root1 = (TreeNode) TreeUtility.createTree(array1, new TreeNode());
@@ -22,24 +22,39 @@ public class CommonNodesInBST {
 	}
 
 	public int solve(TreeNode A, TreeNode B) {
-		ArrayList<Integer> treeA = new ArrayList<>();
-		ArrayList<Integer> treeB = new ArrayList<>();
-		getNodes(A, treeA);
-		getNodes(B, treeB);
 		int result = 0;
-		for (int i = 0; i < treeA.size(); i++) {
-			if (treeB.contains(treeA.get(i)))
-				result += treeA.get(i);
+		Stack<TreeNode> stack1 = new Stack<>();
+		Stack<TreeNode> stack2 = new Stack<>();
+		while (true) {
+			if (A != null) {
+				stack1.push(A);
+				A = A.left;
+			} else if (B != null) {
+				stack2.push(B);
+				B = B.left;
+			} else if (!stack1.isEmpty() && !stack2.isEmpty()) {
+				A = stack1.peek();
+				B = stack2.peek();
+				if (A.val == B.val) {
+					result += A.val;
+					stack1.pop();
+					stack2.pop();
+					A = A.right;
+					B = B.right;
+				}
+				else if (A.val > B.val) {
+					stack2.pop();
+					B = B.right;
+					A = null;
+				} else if(A.val < B.val) {
+					stack1.pop();
+					A = A.right;
+					B = null;
+				}
+			} else
+				break;
 		}
 		return result;
-	}
-
-	private void getNodes(TreeNode root, ArrayList<Integer> list) {
-		if (root == null)
-			return;
-		getNodes(root.left, list);
-		list.add(root.val);
-		getNodes(root.right, list);
 	}
 
 }
