0679390a97189e8680e5b35f8bfdcb18b396c69b
Vasishta Balla <vasishta.balla@gmail.com>
2021-03-06 11:36:16 +0530

    solved KthPermutationSequence efficiently

--- src/com/scaler/academy/homework/oct14/KthPermutationSequence.java
+++ src/com/scaler/academy/homework/oct14/KthPermutationSequence.java
@@ -3,50 +3,41 @@ package com.scaler.academy.homework.oct14;
 // Question: https://www.interviewbit.com/problems/kth-permutation-sequence/
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 
 public class KthPermutationSequence {
 
-    int globalTarget = 0;
-
     public static void main(String[] args) {
         KthPermutationSequence obj = new KthPermutationSequence();
-        System.out.println(obj.getPermutation(3, 5));
+        System.out.println(obj.getPermutation(3, 4));
     }
 
     public String getPermutation(int A, int B) {
         ArrayList<Integer> list = getInitList(A);
-        ArrayList<Integer> aux = new ArrayList<>();
-        HashSet<Integer> hashSet = new HashSet<>();
-        return getPermutationUtil(list, aux, 0, hashSet, B);
-    }
-
-    private String getPermutationUtil(ArrayList<Integer> list, ArrayList<Integer> aux, int index, HashSet<Integer> hashSet, int target) {
-        if (index == list.size()) {
-            globalTarget++;
-            if (globalTarget == target)
-                return getStringFrmList((List<Integer>) aux.clone());
-        }
-        for (int i = 0; i < list.size(); i++) {
-            if (!hashSet.contains(list.get(i))) {
-                hashSet.add(list.get(i));
-                aux.add(list.get(i));
-                String ans = getPermutationUtil(list, aux, index + 1, hashSet, target);
-                if (ans != null)
-                    return ans;
-                hashSet.remove(list.get(i));
-                aux.remove(aux.size() - 1);
-            }
-        }
-        return null;
-    }
-
-    private String getStringFrmList(List<Integer> list) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < list.size(); i++)
-            sb.append(list.get(i));
+        getPermutationUtil(0, A, B - 1, list, sb);
         return sb.toString();
+    }
+
+    public String getPermutationUtil(int index, int size, int target, ArrayList<Integer> list, StringBuilder sb) {
+        if (index == size)
+            return sb.toString();
+        int factorial = factorial(size - index - 1, target);
+        int idx = factorial == 0 ? 0 : target / factorial; // Which index could be fixed now
+        target -= idx * factorial; // Updating the target with already eliminated items
+        sb.append(list.get(idx));
+        list.remove(list.get(idx));
+        return getPermutationUtil(index + 1, size, target, list, sb);
+    }
+
+    private int factorial(int num, int target) {
+        int factorial = 1;
+        while (num > 0) {
+            factorial *= num;
+            num--;
+            if (factorial > target)
+                return 0;
+        }
+        return factorial;
     }
 
     private ArrayList<Integer> getInitList(int size) {
