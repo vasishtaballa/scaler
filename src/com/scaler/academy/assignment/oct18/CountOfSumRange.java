6bc97f72a01c90b76ddc3de85606b2f41d11a0fe
Vasishta Balla <vasishta.balla@gmail.com>
2021-03-17 23:24:49 +0530

    test commit

--- src/com/scaler/academy/assignment/oct18/CountOfSumRange.java
+++ src/com/scaler/academy/assignment/oct18/CountOfSumRange.java
@@ -10,7 +10,7 @@ public class CountOfSumRange {
     public static void main(String[] args) {
         CountOfSumRange obj = new CountOfSumRange();
         int[] array = {-2147483647,0,-2147483647,2147483647};
-        System.out.println(obj.solve((ArrayList<Integer>) ArrayUtils.getListFromArray(array), -564, 3864));
+        System.out.println(obj.solve((ArrayList<Integer>) ArrayUtils.getListFromArray(array), -564, 3863));
     }
 
     public int solve(ArrayList<Integer> A, int B, int C) {
