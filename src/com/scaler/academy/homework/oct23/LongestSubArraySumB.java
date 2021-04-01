fcbcfa6c8059612247457f7e58a11ce196bfe076
Vasishta Balla <vasishta.balla@gmail.com>
2021-04-01 19:45:16 +0530

    solved LongestSubArraySumB problem

--- src/com/scaler/academy/homework/oct23/LongestSubArraySumB.java
+++ src/com/scaler/academy/homework/oct23/LongestSubArraySumB.java
@@ -11,25 +11,25 @@ import java.util.Map;
 public class LongestSubArraySumB {
     public static void main(String[] args) {
         LongestSubArraySumB obj = new LongestSubArraySumB();
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println(obj.solve((ArrayList<Integer>) ArrayUtils.getListFromArray(array), 10));
+        int[] array = {-4, 0, -3, -2, 3, 2, 5, 0, -2, 4};
+        System.out.println(obj.solve((ArrayList<Integer>) ArrayUtils.getListFromArray(array), 2));
     }
 
     public int solve(ArrayList<Integer> A, int B) {
         Map<Integer, Integer> occurrenceIndex = new HashMap<>();
-        occurrenceIndex.put(B, -1);
-        int maxLength = Integer.MIN_VALUE;
-        int sum = 0;
+        occurrenceIndex.put(0, -1);
+        int maxLength = -1, sum = 0, start = -1, end = -1;
         for (int i = 0; i < A.size(); i++) {
             sum += A.get(i);
-            if (occurrenceIndex.containsKey(sum)) {
-                if (i - occurrenceIndex.get(sum) > maxLength) {
-                    maxLength = i - occurrenceIndex.get(sum);
-                }
-            } else {
-                
+            if (sum == B)
+                maxLength = i + 1;
+            if (!occurrenceIndex.containsKey(sum))
+                occurrenceIndex.put(sum, i);
+            if (occurrenceIndex.containsKey(sum - B)) {
+                if (i - occurrenceIndex.get(sum - B) > maxLength)
+                    maxLength = i - occurrenceIndex.get(sum - B);
             }
         }
-        return 0;
+        return maxLength;
     }
 }
