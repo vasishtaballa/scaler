d895621e037fbc7ebc7a3a423489e493129c4b4e
Vasishta Balla <vasishta.balla@gmail.com>
2021-05-09 14:52:55 +0530

    added ImplementStrStr problem

---
+++ src/com/scaler/academy/homework/nov01/ImplementStrStr.java
@@ -0,0 +1,16 @@
+package com.scaler.academy.homework.nov01;
+
+// Question: https://leetcode.com/problems/implement-strstr/
+
+public class ImplementStrStr {
+
+    public static void main(String[] args) {
+        ImplementStrStr obj = new ImplementStrStr();
+        System.out.println(obj.strStr("helloworld", "owo"));
+    }
+
+    public int strStr(final String A, final String B) {
+        return 0;
+    }
+
+}
