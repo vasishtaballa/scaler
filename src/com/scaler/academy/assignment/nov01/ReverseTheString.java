e40fb7ecefcd6ce59a0bb58ceeab49959fce3136
Vasishta Balla <vasishta.balla@gmail.com>
2021-05-09 14:45:12 +0530

    solved ReverseTheString problme

--- src/com/scaler/academy/assignment/nov01/ReverseTheString.java
+++ src/com/scaler/academy/assignment/nov01/ReverseTheString.java
@@ -6,10 +6,21 @@ public class ReverseTheString {
 
     public static void main(String[] args) {
         ReverseTheString obj = new ReverseTheString();
-        System.out.println(obj.solve("the sky is blue"));
+        System.out.println(obj.solve("       fwbpudnbrozzifml osdt ulc jsx kxorifrhubk ouhsuhf sswz qfho dqmy sn myq igjgip iwfcqq                 "));
     }
 
     public String solve(String A) {
-        return null;
+        // Splitting the string based on space character
+        String[] tokens = A.split(" ");
+        StringBuilder result = new StringBuilder();
+        // Iterating each token from right side and appending it to StringBuilder
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            String token = tokens[i].trim();
+            if (!token.isEmpty())
+                result.append(token).append(" ");
+        }
+        // Removing last space which was added on above line for each word
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
     }
 }
