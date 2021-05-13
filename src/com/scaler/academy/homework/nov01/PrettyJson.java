92ea37d86571960d277bd8997e1ba1a004e4ea1f
Vasishta Balla <vasishta.balla@gmail.com>
2021-05-13 22:40:16 +0530

    solved PrettyJson problem

--- src/com/scaler/academy/homework/nov01/PrettyJson.java
+++ src/com/scaler/academy/homework/nov01/PrettyJson.java
@@ -9,71 +9,74 @@ public class PrettyJson {
 
     public static void main(String[] args) {
         PrettyJson obj = new PrettyJson();
-        String[] result = obj.prettyJSON("{\"attributes\":[{\"nm\":\"ACCOUNT\",\"lv\":[{\"v\":{\"Id\":null,\"State\":null},\"vt\":\"java.util.Map\",\"cn\":1}],\"vt\":\"java.util.Map\",\"status\":\"SUCCESS\",\"lmd\":13585},{\"nm\":\"PROFILE\",\"lv\":[{\"v\":{\"Party\":null,\"Ads\":null},\"vt\":\"java.util.Map\",\"cn\":2}],\"vt\":\"java.util.Map\",\"status\":\"SUCCESS\",\"lmd\":41962}]}");
-        for (String str : result) {
+        String[] result = obj.prettyJSON("{\"id\":100,\"firstName\":\"Jack\",\"lastName\":\"Jones\",\"age\":12}");
+        for (String str : result)
             System.out.println(str);
-        }
     }
 
     public String[] prettyJSON(String A) {
         List<String> result = new ArrayList<>();
-        String[] tokens = A.split(",");
-        int tabs = 0;
-        boolean isSameLineComma = false;
         StringBuilder onGoingString = new StringBuilder();
-        for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i].trim();
-            isSameLineComma = false;
-            if (!token.isEmpty()) {
-                int index = 0;
-                while (index < token.length()) {
-                    char ch = token.charAt(index);
-                    if (ch == '{' || ch == '[') {
-                        if (onGoingString.length() > 0) {
-                            String withTabs = prefixWithTabs(onGoingString.toString(), tabs);
-                            if (!withTabs.isEmpty())
-                                result.add(withTabs);
-                            onGoingString.delete(0, onGoingString.length());
-                        }
-                        String withTabs = prefixWithTabs(String.valueOf(ch), tabs);
-                        if (!withTabs.isEmpty())
-                            result.add(withTabs);
-                        tabs++;
-                    } else if (ch == '}' || ch == ']') {
-                        if (onGoingString.length() > 0) {
-                            String withTabs = prefixWithTabs(onGoingString.toString(), tabs);
-                            if (!withTabs.isEmpty())
-                                result.add(withTabs);
-                            onGoingString.delete(0, onGoingString.length());
-                        }
-                        tabs--;
-                        String withTabs = prefixWithTabs(String.valueOf(ch), tabs);
-                        if (index < token.length() - 1 && (token.charAt(index + 1) == '}' || token.charAt(index + 1) == ']')) {
-                            index++;
-                        } else if (i < tokens.length - 1 && (tokens[i + 1].charAt(0) != '}' && tokens[i + 1].charAt(0) != ']')) {
-                            withTabs = withTabs + ',';
-                            isSameLineComma = true;
-                        }
-                        if (!withTabs.isEmpty())
-                            result.add(withTabs);
-                    } else {
-                        onGoingString.append(ch);
-                    }
-                    index++;
+        int tabs = 0;
+        for (int i = 0; i < A.length(); i++) {
+            char ch = A.charAt(i);
+            if (ch == '{' || ch == '[') {
+                if (i < A.length() - 1 && A.charAt(i + 1) != ',') {
+                    addToResult(onGoingString.toString(), tabs, result);
+                    flushOnGoingString(onGoingString);
+                    addToResult(String.valueOf(ch), tabs, result);
+                } else if (i < A.length() - 1 && A.charAt(i + 1) == ',') {
+                    addToResult(onGoingString.toString() + ',', tabs, result);
+                    flushOnGoingString(onGoingString);
+                    addToResult(String.valueOf(ch), tabs, result);
+                    i++;
                 }
-                if (i != tokens.length - 1 && !isSameLineComma)
-                    onGoingString.append(",");
-                String withTabs = prefixWithTabs(onGoingString.toString(), tabs);
-                if (!withTabs.isEmpty() && !isSameLineComma)
-                    result.add(withTabs);
-                onGoingString.delete(0, onGoingString.length());
+                tabs++;
+            } else if (ch == '}' || ch == ']') {
+                if (i < A.length() - 1 && A.charAt(i + 1) != ',') {
+                    addToResult(onGoingString.toString(), tabs, result);
+                    flushOnGoingString(onGoingString);
+                    tabs--;
+                    addToResult(String.valueOf(ch), tabs, result);
+                } else if (i < A.length() - 1 && A.charAt(i + 1) == ',') {
+                    addToResult(onGoingString.toString(), tabs, result);
+                    flushOnGoingString(onGoingString);
+                    tabs--;
+                    addToResult(String.valueOf(ch) + ',', tabs, result);
+                    i++;
+                } else {
+                    addToResult(onGoingString.toString(), tabs, result);
+                    flushOnGoingString(onGoingString);
+                    tabs--;
+                    addToResult(String.valueOf(ch), tabs, result);
+                }
+            } else if (ch == ',') {
+                onGoingString.append(ch);
+                addToResult(onGoingString.toString(), tabs, result);
+                flushOnGoingString(onGoingString);
+            } else {
+                onGoingString.append(ch);
             }
         }
-        String[] array = new String[result.size()];
-        int i = 0;
+        return getResultInArray(result);
+    }
+
+    private void flushOnGoingString(StringBuilder onGoingString) {
+        onGoingString.delete(0, onGoingString.length());
+    }
+
+    private void addToResult(String string, int tabs, List<String> result) {
+        String withTabs = prefixWithTabs(string, tabs);
+        if (!withTabs.trim().isEmpty())
+            result.add(withTabs);
+    }
+
+    private String[] getResultInArray(List<String> result) {
+        String[] resultArray = new String[result.size()];
+        int index = 0;
         for (String s : result)
-            array[i++] = s;
-        return array;
+            resultArray[index++] = s;
+        return resultArray;
     }
 
     private String prefixWithTabs(String value, int tabs) {
