1c355d7e5d5fbdeb4d0e7a2f8d7ca77894a50133
Vasishta Balla <vasishta.balla@gmail.com>
2021-05-15 13:59:22 +0530

    explanation for RedundantBraces problem

--- src/com/scaler/academy/homework/nov06/RedundantBraces.java
+++ src/com/scaler/academy/homework/nov06/RedundantBraces.java
@@ -6,28 +6,36 @@ import java.util.Stack;
 
 public class RedundantBraces {
 
-	public static void main(String[] args) {
-		RedundantBraces obj = new RedundantBraces();
-		System.out.println(obj.braces("((a+b))"));
-	}
+    public static void main(String[] args) {
+        RedundantBraces obj = new RedundantBraces();
+        System.out.println(obj.braces("((a+b))"));
+    }
 
-	public int braces(String A) {
-		Stack<Character> stack = new Stack<>();
-		for (int i = 0; i < A.length(); i++) {
-			if (A.charAt(i) == ')') {
-				boolean operatorFlag = false;
-				while (stack.peek() != '(') {
-					char top = stack.pop();
-					if (top == '+' || top == '-' || top == '*' || top == '/')
-						operatorFlag = true;
-				}
-				if (operatorFlag == false)
-					return 1;
-				stack.pop();
-			} else
-				stack.push(A.charAt(i));
-		}
-		return 0;
-	}
+    /*
+    The question was just about checking if un-necessary braces are there or not.
+    Ex:
+        1. a + (b * c) -> No redundant braces
+        2. a + (b) / c -> Redundant braces are there as braces surrounding to b are not required
+    The solution was just checking if any open closed braces are there without any operator like +, -, *, /
+    If any open and closed braces comes without any operator, then redundant braces are there. Else, no
+     */
+    public int braces(String A) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) == ')') {
+                boolean operatorFlag = false;
+                while (stack.peek() != '(') {
+                    char top = stack.pop();
+                    if (top == '+' || top == '-' || top == '*' || top == '/')
+                        operatorFlag = true;
+                }
+                if (operatorFlag == false)
+                    return 1;
+                stack.pop();
+            } else
+                stack.push(A.charAt(i));
+        }
+        return 0;
+    }
 
 }
