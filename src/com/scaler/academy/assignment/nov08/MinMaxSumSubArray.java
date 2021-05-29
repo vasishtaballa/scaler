4ab4798793daa7ab8b90b24443f162dcf7429278
Vasishta Balla <vasishta.balla@gmail.com>
2021-05-29 22:02:20 +0530

    solved MinMaxSumSubArray problem

--- src/com/scaler/academy/assignment/nov08/MinMaxSumSubArray.java
+++ src/com/scaler/academy/assignment/nov08/MinMaxSumSubArray.java
@@ -7,43 +7,62 @@ import java.util.LinkedList;
 
 public class MinMaxSumSubArray {
 
-	public static void main(String[] args) {
-		MinMaxSumSubArray obj = new MinMaxSumSubArray();
-		int[] array = { 2, 5, -1, 7, -3, -1, -2 };
-		System.out.println(obj.solve(array, 4));
-	}
+    public static void main(String[] args) {
+        MinMaxSumSubArray obj = new MinMaxSumSubArray();
+        int[] array = {2, -1, 3};
+        System.out.println(obj.solve(array, 2));
+    }
 
-	public int solve(int[] A, int B) {
-		int result = 0, minimum = Integer.MAX_VALUE, maximum = Integer.MIN_VALUE, modValue = 1000000007;
-		Deque<Integer> maxQueue = new LinkedList<>();
-		Deque<Integer> minQueue = new LinkedList<>();
-		maxQueue.addLast(0);
-		minQueue.addLast(0);
-		for (int i = 1; i < B; i++) {
-			while (!maxQueue.isEmpty() && A[maxQueue.peekLast()] <= A[i])
-				maxQueue.pollLast();
-			maxQueue.addLast(i);
-			while (!minQueue.isEmpty() && A[minQueue.peekLast()] >= A[i])
-				minQueue.pollLast();
-			minQueue.addLast(i);
-		}
-		for (int i = B; i < A.length; i++) {
-			maximum = A[maxQueue.peekFirst()];
-			minimum = A[minQueue.peekFirst()];
-			result += maximum % modValue + minimum % modValue;
-			while (B - i >= maxQueue.peekFirst())
-				maxQueue.pollFirst();
-			while (B - i >= minQueue.peekFirst())
-				minQueue.pollFirst();
-			while (!maxQueue.isEmpty() && A[maxQueue.peekLast()] <= A[i])
-				maxQueue.pollLast();
-			maxQueue.addLast(i);
-			while (!minQueue.isEmpty() && A[minQueue.peekLast()] >= A[i])
-				minQueue.pollLast();
-			minQueue.addLast(i);
-		}
-		result += maximum % modValue + minimum % modValue;
-		return result;
-	}
+    /*
+    The idea is to use two queues. One queue to store minimum number so far and another queue to store maximum number so far (to achieve the min and max for each sub-array in an optimal way)
+     */
+
+    public int solve(int[] A, int B) {
+        long result = 0;
+        int modValue = 1000000007;
+        Deque<Integer> maxQueue = new LinkedList<>();
+        Deque<Integer> minQueue = new LinkedList<>();
+        int i = 0;
+        // Iterating first B characters and updating the min & max queue
+        for (; i < B; i++) {
+            /*
+            While the last element in max queue is smaller than current element, poll all the elements and at the end push the current element
+            This is because, if any smaller numbers are there, they will be overridden by the current element while nominating to become max
+             */
+            while (!maxQueue.isEmpty() && A[i] >= A[maxQueue.peekLast()])
+                maxQueue.pollLast();
+            maxQueue.add(i);
+            while (!minQueue.isEmpty() && A[i] <= A[minQueue.peekLast()])
+                minQueue.pollLast();
+            minQueue.add(i);
+        }
+        // Get the min and max for first sub-array of size B and add them to the result
+        result += A[maxQueue.peekFirst()] + A[minQueue.peekFirst()];
+        result %= modValue;
+        // Iterating rest of the elements after B elements
+        while (i < A.length) {
+            /*
+            Poll elements from both the queues which should not be included in the current sub-array
+            We can find this by using the ith pointer and subtracting B from i will give the start point of the current sub-array.
+            If any of the indexes outside this range are present in any of the queue, we should remove them
+             */
+            while (!maxQueue.isEmpty() && i - B >= maxQueue.peekFirst())
+                maxQueue.pollFirst();
+            while (!minQueue.isEmpty() && i - B >= minQueue.peekFirst())
+                minQueue.pollFirst();
+            while (!maxQueue.isEmpty() && A[i] >= A[maxQueue.peekLast()])
+                maxQueue.pollLast();
+            maxQueue.add(i);
+            while (!minQueue.isEmpty() && A[i] <= A[minQueue.peekLast()])
+                minQueue.pollLast();
+            minQueue.add(i);
+            result += A[maxQueue.peekFirst()] + A[minQueue.peekFirst()];
+            result %= modValue;
+            i++;
+        }
+        result += modValue;
+        result %= modValue;
+        return (int) result;
+    }
 
 }
